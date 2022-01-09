c1ec5df7c19cf1d0505505b4753edb4b711228ec
Artem <31787423+DokSna@users.noreply.github.com>
2022-01-09 13:39:44 +0300

    Определение класса и объекта

--- bin/classys.dart
+++ bin/classys.dart
@@ -1,5 +1,26 @@
 void main() {
   Car myCar = Car();
+  Car myCar2 = Car();
+
+  myCar.carName = 'BMW';
+  myCar.color = 'Black';
+  myCar.start();
+
+  myCar2.carName = 'Ford';
+  myCar2.color = 'Blue';
+  myCar2.start();
+  myCar2.stop();
 }
 
-class Car {}
+class Car {
+  late String carName;
+  var color;
+
+  void start() {
+    print('The $color $carName has started.');
+  }
+
+  void stop() {
+    print('The $carName has stopped now.');
+  }
+}
