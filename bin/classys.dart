d065c21451141a982c9482db90b1cc591c050cfb
Artem <31787423+DokSna@users.noreply.github.com>
2022-01-16 12:37:57 +0300

    Классы, Объекты, именованные конструкторы

--- bin/classys.dart
+++ bin/classys.dart
@@ -1,4 +1,5 @@
 void main() {
+  /*
   Car myCar = Car();
   Car myCar2 = Car();
 
@@ -10,12 +11,39 @@ void main() {
   myCar2.color = 'Blue';
   myCar2.start();
   myCar2.stop();
+  */
+  Car myCar3 = Car('Toyota', color: 'Green');
+  myCar3.start();
+  Car myCar4 = Car('Mazda');
+  myCar4.start();
+  Car myCar5 = Car.named();
+  myCar5.start();
+  Car myCar0 = Car.fromColor('black');
+  myCar0.start();
 }
 
 class Car {
   late String carName;
   var color;
-
+  // int powers;
+// именованые конструкторы
+  Car.named() {
+    carName = 'Ford';
+    color = 'Blue';
+  }
+  Car.fromColor(var coloric) {
+    carName = 'RENO';
+    this.color = coloric;
+  }
+  // Конструктор
+  // Car(this.carName, [this.color = 'Red']);
+  Car(this.carName, {this.color = 'Red'});
+/*
+  Car(String carName, var color) {
+    this.carName = carName;
+    this.color = color;
+  }
+ */
   void start() {
     print('The $color $carName has started.');
   }
