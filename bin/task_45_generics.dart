c54377e5dfb765e335b363563f3da1eaaf85e061
Artem <31787423+DokSna@users.noreply.github.com>
2022-03-10 21:21:04 +0300

    Обобщения (Generics) дженерики

---
+++ bin/task_45_generics.dart
@@ -0,0 +1,25 @@
+void main() {
+  Car car = Car("2", "BMW");
+  print(car.id.runtimeType);
+  car.move("100");
+
+  print('');
+
+  Car car1 = Car(1, "Ford");
+  print(car1.id.runtimeType);
+  car1.move(101);
+
+  // int id = car.id;
+  // print(id);
+}
+
+class Car<T> {
+  T id;
+  String carName;
+
+  Car(this.id, this.carName);
+
+  void move<T>(T speed) {
+    print("The $carName. ID number: $id. Speed car: $speed");
+  }
+}
