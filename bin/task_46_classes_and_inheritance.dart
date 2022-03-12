d4ac95fb8aa212b2e50c91fe2246cab67565af9e
Artem <31787423+DokSna@users.noreply.github.com>
2022-03-12 19:22:17 +0300

    46. ДЗ по классам и наследованию
    Задание выполнено! : )

--- bin/task_46_classes_and_inheritance.dart
+++ bin/task_46_classes_and_inheritance.dart
@@ -1,33 +1,70 @@
 void main() {
-  var cuboid_1 = Cuboid(1, 2, 3);
-  cuboid_1.volume;
-  cuboid_1.surfaceArea;
+  var cuboid = Cuboid(1, 2, 3);
+  cuboid.volume;
+  cuboid.surfaceArea;
+
+  var cube = Cube(2);
+  cube.volume;
+  cube.surfaceArea;
 }
 
 class Cuboid {
   // свойства
-  // late int length, width, height; // length, width и height
   late int length;
   late int width;
   late int height;
+  String nameCub = 'Cuboid';
 
   // конструктор - инициализирует свойства объекста
   Cuboid(this.length, this.width, this.height);
 
   // Площадь прямоугольного параллелепипеда S = 2(a · b + a · h + b · h)
-
   void get surfaceArea {
     int _surfaceArea = 2 * (length * width + length * height + width * height);
-    print('Cuboid Surface Area = $_surfaceArea');
+    print('$nameCub Surface Area = $_surfaceArea \n');
   }
 
   // объём кубоида Volume = length * width * height
   void get volume {
     int _volume = length * width * height;
-    print('Cuboid Volume = $_volume');
+    print('$nameCub Volume = $_volume \n');
+  }
+}
+
+class Cube extends Cuboid {
+  Cube(length) : super(length, length, length) {
+    this.nameCub = 'Cube';
+  }
+}
+
+
+/*
+
+{
+    this.length = super.length;
+    this.width = super.length;
+    this.height = super.length;
+  }
+// Формула площади куба: S = 6 * a^2
+  void get surfaceArea {
+    num _surfaceArea = 6 * length * length;
+    print('Cube Surface Area = $_surfaceArea');
   }
 
+  void get volume {
+    int _volume = length * length * length;
+    print('Cube Volume = $_volume');
+  }
+  ------
+  Cuboid(int length, [int? width, int? height]) {
+    this.length = length;
+    this.width = width == null ? length: width;
+    this.height = height == null ? length: height;
+  }
+
+  
   void infoCub() {
     print('Длина: $length, ширина: $width, высота: $height.');
   }
-}
+
+*/
