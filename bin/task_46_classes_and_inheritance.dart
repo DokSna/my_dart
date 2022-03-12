a4e4aba07165019e6ca7b47939c804a4ba1a397a
Artem <31787423+DokSna@users.noreply.github.com>
2022-03-12 13:15:31 +0300

    Выводит объём и площадь куба с 3-мя аргументами

--- bin/task_46_classes_and_inheritance.dart
+++ bin/task_46_classes_and_inheritance.dart
@@ -1,8 +1,7 @@
 void main() {
   var cuboid_1 = Cuboid(1, 2, 3);
-  cuboid_1.infoCub();
-  print(cuboid_1.surfaceArea);
-  print(cuboid_1.volume);
+  cuboid_1.volume;
+  cuboid_1.surfaceArea;
 }
 
 class Cuboid {
@@ -15,20 +14,20 @@ class Cuboid {
   // конструктор - инициализирует свойства объекста
   Cuboid(this.length, this.width, this.height);
 
-  void infoCub() {
-    print('Длина: $length, ширина: $width, высота: $height.');
-  }
-
   // Площадь прямоугольного параллелепипеда S = 2(a · b + a · h + b · h)
-  // SurfaceArea
 
-  int get surfaceArea {
+  void get surfaceArea {
     int _surfaceArea = 2 * (length * width + length * height + width * height);
-    return _surfaceArea;
+    print('Cuboid Surface Area = $_surfaceArea');
   }
 
   // объём кубоида Volume = length * width * height
-  int get volume {
-    return length * width * height;
+  void get volume {
+    int _volume = length * width * height;
+    print('Cuboid Volume = $_volume');
+  }
+
+  void infoCub() {
+    print('Длина: $length, ширина: $width, высота: $height.');
   }
 }
