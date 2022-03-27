6361c579555b22167a762093bb360664d4e81d23
Artem <31787423+DokSna@users.noreply.github.com>
2022-03-27 15:45:25 +0300

    55. Каскадный null-aware оператор
    Complete

--- bin/task_55.dart
+++ bin/task_55.dart
@@ -3,11 +3,25 @@
 // ?.. - cascade null-aware operator
 
 void main() {
+  // Null
+  Path? pathNull;
+
+  pathNull
+    ?..moveTo(0, 0)
+    ..lineTo(0, 2)
+    ..lineTo(2, 2);
+
+  // non-Null
   Path path = Path();
 
-  path.moveTo(0, 0);
-  path.lineTo(0, 2);
-  path.lineTo(2, 2);
+  path
+    ..moveTo(0, 0)
+    ..lineTo(0, 2)
+    ..lineTo(2, 2);
+
+  // path.moveTo(0, 0);
+  // path.lineTo(0, 2);
+  // path.lineTo(2, 2);
 }
 
 class Path {
